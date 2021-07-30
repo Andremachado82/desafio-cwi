7f5152e1e9289f89e7c86a8be7102763825ea0a4
Andremachado82 <andrels.machado82@gmail.com>
2021-07-29 21:52:07 -0300

    Atualizado forma de pegar as variáveis pelo apllication.properties.

--- src/main/java/com/desafio/cwi/config/SwaggerConfig.java
+++ src/main/java/com/desafio/cwi/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.desafio.cwi.config;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +10,6 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +17,33 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	
+	@Value("${swagger.title}")
+	private String title;
+	
+	@Value("${swagger.description}")
+	private String description;
+	
+	@Value("${swagger.version}")
+	private String version;
+	
+	@Value("${swagger.terms}")
+	private String terms;
+	
+	@Value("${swagger.develloper-name}")
+	private String develloperName;
+	
+	@Value("${swagger.develloper-url}")
+	private String develloperUrl;
+	
+	@Value("${swagger.develloper-email}")
+	private String develloperEmail;
+	
+	@Value("${swagger.license}")
+	private String license;
+	
+	@Value("${swagger.license-url}")
+	private String licenseUrl;
 
 	@Bean
 	public Docket api() {
@@ -30,12 +57,11 @@ public class SwaggerConfig {
 	private ApiInfo metaInfo() {
 
 		ApiInfo apiInfo = new ApiInfo(
-				"API REST DESAFIO TÉCNICO CWI", 
-				"API REST de um sistema de gerenciamento de votação de associados em assembléia",
-				"1.0", "Terms of Service",
-				new Contact("André Machado", "https://www.linkedin.com/in/andrelsmachado",
-						"andrels.machado82@gmail.com"),
-				"Apache License Version 2.0", "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>());
+				title, 
+				description,
+				version, terms,
+				new Contact(develloperName, develloperUrl ,develloperEmail),
+				license, licenseUrl, new ArrayList<>());
 
 		return apiInfo;
 	}
--- src/main/java/com/desafio/cwi/recursos/CpfValidationClient.java
+++ src/main/java/com/desafio/cwi/recursos/CpfValidationClient.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url="https://user-info.herokuapp.com/users/", name="cpf")
+//@FeignClient(url="https://user-info.herokuapp.com/users/", name="cpf")
+@FeignClient(url="${client.applicationName.url.cpf}", name="cpf")
 public interface CpfValidationClient {
 	
 	@GetMapping("/{cpf}")
