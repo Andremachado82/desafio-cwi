b62b1dcda8850e04a7cac2d15ec489480a7bbb31
Andremachado82 <andrels.machado82@gmail.com>
2021-08-07 20:01:28 -0300

    Testes de exceções ao salvar voto quando sessão expirada, cpf for nulo e cpf não tiver permissão para votar

--- src/test/java/com/desafio/cwi/services/VotoTestes/VotoCreateServiceTest.java
+++ src/test/java/com/desafio/cwi/services/VotoTestes/VotoCreateServiceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Random;
 
@@ -14,14 +15,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.desafio.cwi.client.CpfValidationClient;
 import com.desafio.cwi.exceptions.ApiGenericException;
+import com.desafio.cwi.exceptions.CpfNotFoundException;
 import com.desafio.cwi.exceptions.PautaNotFoundException;
+import com.desafio.cwi.exceptions.SessionExperidException;
+import com.desafio.cwi.exceptions.UnableCpfException;
 import com.desafio.cwi.models.Pauta;
 import com.desafio.cwi.models.Sessao;
 import com.desafio.cwi.models.Voto;
 import com.desafio.cwi.repositories.PautaRepository;
 import com.desafio.cwi.repositories.SessaoRepository;
 import com.desafio.cwi.repositories.VotoRepository;
+import com.desafio.cwi.responses.CpfValidationResponse;
 import com.desafio.cwi.services.voto.VotoCreateService;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -35,6 +41,9 @@ public class VotoCreateServiceTest {
 	
 	@Mock
 	private SessaoRepository sessaoRepository;
+	
+	@Mock
+	private CpfValidationClient cpfValidationClient;
 	
 	@Mock
 	private VotoRepository sessaoVotacaoRepository;
@@ -77,6 +86,70 @@ public class VotoCreateServiceTest {
 		votoCreateService.execute(1l, voto);
 
 		
+		verify(sessaoVotacaoRepository, never()).save(any(Voto.class));
+	}
+	
+	@Test(expected = SessionExperidException.class)
+	public void deveOcorrerErroAoSalvarQuandoSessaoForExpirada() {
+		
+		Voto voto = getVoto();
+		voto.setResposta(true);
+		voto.setCpf(null);
+		
+		Optional<Pauta> pautaSalva = Optional.ofNullable(getPauta());
+		when(pautaRepository.findById(voto.getPauta().getId())).thenReturn(pautaSalva);
+		
+		Optional<Sessao> sessaoSalva = Optional.ofNullable(getSessao());
+		sessaoSalva.get().setDataHoraInicio(LocalDateTime.now().plusMinutes(-30));
+		sessaoSalva.get().setTempoSessao(1l);
+		when(sessaoRepository.findById(1l)).thenReturn(sessaoSalva);
+		
+		votoCreateService.execute(1l, voto);
+
+		verify(sessaoVotacaoRepository, never()).save(any(Voto.class));
+	}
+	
+	@Test(expected = CpfNotFoundException.class)
+	public void deveOcorrerErroAoSalvarQuandoCpfForNulo() {
+		
+		Voto voto = getVoto();
+		voto.setResposta(true);
+		voto.setCpf(null);
+		
+		Optional<Pauta> pautaSalva = Optional.ofNullable(getPauta());
+		when(pautaRepository.findById(voto.getPauta().getId())).thenReturn(pautaSalva);
+		
+		Optional<Sessao> sessaoSalva = Optional.ofNullable(getSessao());
+		sessaoSalva.get().setDataHoraInicio(LocalDateTime.now().plusMinutes(30));
+		sessaoSalva.get().setTempoSessao(1l);
+		when(sessaoRepository.findById(1l)).thenReturn(sessaoSalva);
+		
+		votoCreateService.execute(1l, voto);
+
+		verify(sessaoVotacaoRepository, never()).save(any(Voto.class));
+	}
+	
+	@Test(expected = UnableCpfException.class)
+	public void deveOcorrerErroAoSalvarQuandoApiCpfRetornarUNABLE_TO_VOTE() {
+		
+		Voto voto = getVoto();
+		voto.setResposta(true);
+		voto.setCpf("52990556095");
+		
+		Optional<Pauta> pautaSalva = Optional.ofNullable(getPauta());
+		when(pautaRepository.findById(voto.getPauta().getId())).thenReturn(pautaSalva);
+		
+		Optional<Sessao> sessaoSalva = Optional.ofNullable(getSessao());
+		sessaoSalva.get().setDataHoraInicio(LocalDateTime.now().plusMinutes(30));
+		sessaoSalva.get().setTempoSessao(1l);
+		when(sessaoRepository.findById(1l)).thenReturn(sessaoSalva);
+		
+		CpfValidationResponse cpfResponse = new CpfValidationResponse();
+		cpfResponse.setStatus("UNABLE_TO_VOTE");
+		when(cpfValidationClient.findUserByCpf(voto.getCpf())).thenReturn(cpfResponse);
+		
+		votoCreateService.execute(1l, voto);
+
 		verify(sessaoVotacaoRepository, never()).save(any(Voto.class));
 	}
 	
